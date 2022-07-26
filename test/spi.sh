82cf870ce71218f7356908107af490f24b6a1efc
Da Xue <da@lessconfused.com>
2022-07-26 19:43:02 -0400

    spi test use isActive

--- test/spi.sh
+++ test/spi.sh
@@ -5,9 +5,18 @@ if [ "$USER" != "root" ]; then
 	exit 1
 fi
 
-VENDOR=libre-computer BOARD=aml-s905x-cc ./ldto enable spicc-cs1
-VENDOR=libre-computer BOARD=aml-s905x-cc ./ldto enable spidev-spicc-cs1
-sleep 1
+if ! ./ldto isActive spicc-cs1; then
+	./ldto enable spicc-cs1
+	sleep 1
+fi
+if ./ldto isActive spidev-spicc-cs1; then
+	./ldto disable spidev-cc-cs1
+	sleep 1
+fi
+if ! ./ldto isActive spidev-spicc-cs1; then
+	./ldto enable spidev-spicc-cs1
+	sleep 1
+fi
 echo spidev > /sys/bus/spi/devices/spi0.0/driver_override
 echo spidev > /sys/bus/spi/devices/spi0.1/driver_override
 sleep 1
