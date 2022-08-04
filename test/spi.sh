b18f86b45140bffc567b13a79d557faaeca45735
Da Xue <da.xue@libretech.co>
2022-08-04 12:31:06 -0400

    test/spi: controllable spi test

--- test/spi.sh
+++ test/spi.sh
@@ -5,38 +5,68 @@ if [ "$USER" != "root" ]; then
 	exit 1
 fi
 
-if ! ./ldto isActive spicc-cs1; then
-	./ldto enable spicc-cs1
-	sleep 1
+if [ -z "$1" ]; then
+	echo "$0 SPIDEVNUM" >&2
+	exit 1
 fi
-if ./ldto isActive spidev-spicc-cs1; then
-	./ldto disable spidev-spicc-cs1
-	sleep 1
-fi
-if ! ./ldto isActive spidev-spicc-cs1; then
-	./ldto enable spidev-spicc-cs1
-	sleep 1
-fi
-echo spidev > /sys/bus/spi/devices/spi0.0/driver_override
-echo spidev > /sys/bus/spi/devices/spi0.1/driver_override
-sleep 1
-echo spi0.0 > /sys/bus/spi/drivers/spidev/bind
-echo spi0.1 > /sys/bus/spi/drivers/spidev/bind
-sleep 1
-for spic in 0 1; do
-	for speed in 500000 1000000 2000000 4000000 8000000 16000000; do
-		for mode in 0 1 2 3; do
-			dev=/dev/spidev0.$spic
-			count=$((speed * 2048 / 1000 / 1000))
-			size=$((count >> 1))
-			echo
-			echo "Testing $dev in mode $mode at speed $speed Hz with $size KB"
-			echo
-			spi-config -d $dev -m $mode -s $speed -w &
-			pid=$!
-			tr '\0' '4' < /dev/zero | dd bs=512 count=$count of=$dev
-			kill $pid
-		done
-	done
+
+SPIDEVNUM="$1"
+SPICSMAX="0"
+while true; do
+	if [ ! -e "/dev/spidev${SPIDEVNUM}.${SPICSMAX}" ]; then
+		if [ "$SPICSMAX" -eq 0 ]; then
+			echo "SPIDEV $SPIDEV does not exist." >&2
+			exit 1
+		fi
+		break
+	fi
+	((SPICSMAX++))
+done
+echo "SPIDEV $0 has $SPICSMAX chip enable(s)." >&2
+
+freq=500000
+chip=0
+mode=0
+data='S'
+block=512
+stop=0
+
+echo "Press q to quit, m to change mode, c to change chip select, w and s to double/half freq." >&2
+
+while [ $stop -eq 0 ]; do
+	echo "Testing SPI ${SPIDEVNUM} CHIP ${chip} MODE ${mode} @ ${freq}Hz ." >&2
+	dev=/dev/spidev$SPIDEVNUM.$chip
+	spi-config -d $dev -m $mode -s $freq -w &
+	spi_config_pid=$!
+	tr '\0' "$data" < /dev/zero | dd bs=$block of=$dev &
+	dd_pid=$!
+	read -n 1 ctrl_char
+	kill $dd_pid
+	kill $spi_config_pid
+	case "$ctrl_char" in
+		q)
+			stop=1
+			break
+			;;
+		m)
+			mode=$((++mode % 4))
+			;;
+		c)
+			chip=$((++chip % SPICSMAX))
+			;;
+		w)
+			freq=$((freq << 1))
+			;;
+		s)
+			if [ $freq -lt 31250 ]; then
+				echo "Frequency is already at minimum." >&2
+			else
+				freq=$((freq >> 1))
+			fi
+			;;	
+		*)
+			echo "Unrecognized command character $ctrl_char." >&2
+			;;
+	esac
 done
 
