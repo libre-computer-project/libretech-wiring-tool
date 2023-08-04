f3b4e22caabf209f2592c334caaaae8a7579a2f0
Da Xue <da.xue@libretech.co>
2023-08-04 02:52:47 -0400

    build: create changelog

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+dateymd=$(date +%Y.%m.%d)
+commit=$(git rev-parse HEAD)
+dateutc=$(date -Ru)
+cat <<EOF > debian/changelog
+libretech-wiring-tool ($dateymd) linux; urgency=medium
+
+  * $commit
+
+ -- Da Xue <da@libre.computer>  $dateutc
+EOF
+dpkg-buildpackage -uc --no-sign --build=all
